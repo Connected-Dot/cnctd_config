8bc8dbc04e5fa9014d97bf3cd8f9528acc936165
kebtech <64436886+kebtech@users.noreply.github.com>
2024-01-15 11:49:02 -0500

    built main functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,43 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use serde::{Serialize, Deserialize};
+use std::fs::{File, OpenOptions};
+use std::io::{Read, Write, Error, self};
+use std::path::Path;
+
+pub struct Config<T> where T: Serialize + for<'de> Deserialize<'de> {
+    pub data: T,
+    pub file_path: String,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl<T> Config<T> where T: Serialize + for<'de> Deserialize<'de> {
+    pub fn new(data: T, file_path: Option<&str>) -> Self {
+        Config {
+            data,
+            file_path: if file_path.is_some() { file_path.unwrap().to_string() } else { "./config.json".to_string() },
+        }
+    }
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    pub fn update(&mut self, new_data: T) -> Result<(), Error> {
+        self.data = new_data;
+        self.save()
+    }
+
+    pub fn get(&self) -> &T {
+        &self.data
+    }
+
+    pub fn save(&self) -> Result<(), Error> {
+        let mut file = File::create(&self.file_path)?;
+        let data_as_json = serde_json::to_string(&self.data)?;
+        file.write_all(data_as_json.as_bytes())?;
+        Ok(())
+    }
+
+    pub fn load(&mut self) -> io::Result<()> {
+        let mut file = File::open(&self.file_path)?;
+        let mut data = String::new();
+        file.read_to_string(&mut data)?;
+        self.data = serde_json::from_str(&data)?;
+        Ok(())
     }
 }
+
